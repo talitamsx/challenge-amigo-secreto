96712676649006b701d2a5e92314d8ef9a821106
talita <talitamsx@gmail.com>
2025-02-04 12:15:49 -0300

    adicionando nomes e mostrando na tela

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,21 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let lista = [];
 let nome;
-console.log(lista);
-function adicionarAmigo() {
-  nome = document.querySelector("input").value; //pega valor de input
-  if (nome.trim() !== "") {
-    //verifica se o campo esta vazio
-    lista.push(nome); //adiciona nome a lista
 
+// busca o elemento que possui o id igual a "listaAmigos"
+//  Que é uma lista <ul> onde os nomes serão adicionados
+let listaAmigos = document.getElementById("listaAmigos");
+//console.log(lista);
+
+// Adiciona nome à lista de amigos, exibe na tela e limpa o campo de input.
+function adicionarAmigo() {
+  nome = document.querySelector(".input-name").value; //pega valor de input
+  let nomeAdicionado = document.createElement("li"); //cria um elemento <li> para listar os nomes
+
+  //verifica se o campo esta vazio
+  if (nome.trim() !== "") {
+    lista.push(nome); //adiciona nome a lista
+    nomeAdicionado.textContent = nome; // recebe o valor para mostrar na tela
     console.log(lista);
+    listaAmigos.appendChild(nomeAdicionado); //mostra na tela o valor digitado
   } else {
     alert("Digite um nome");
   }
   limparCampo();
 }
+
+//limpa campo após inserir nome
 function limparCampo() {
-  nome = document.querySelector("input");
+  nome = document.querySelector(".input-name");
   nome.value = "";
 }
 
